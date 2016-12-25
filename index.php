7cab1bfcaf3a198c5104cda3cb7dcdb3bdf28578
Matt Thompson <matt.thompson@stack.com>
2016-12-25 00:24:27 -0500

    everybody but jess

--- index.php
+++ index.php
@@ -28,18 +28,38 @@
 
 						<p>Another year, another letter.  I do hope that you both enjoy receiving this gift as much as I enjoy writing it.  </p>
 
+						<p>I know that you both have had HUGE years this year!  From Uncle David (lead broadway roles, stark county most famous, etc..) to Aunt Kathy (traveling the world, on her 3rd grandchild in as many years (fact check?), etc..) I am inspired by the role models that you both are, as well as the success that you both have had.</p>
+
+						<p>I would like to share a little bit about <i>our</i> year.  The good, the bad, and the ugly ..</p>
+
 						<p><strong><u>Maci</u></strong></p>
-						<p>Maci has had an exciting year in 2016</p>
+						<p>Maci has had an exciting year in 2016.  This year, she started kindergarten.  Kindergarten has been very different from preschool.  Instead of going to school for 3 hours per day, she now goes for 8 hours per day.  She has been loving every minute of it.  Her favorite subjects are Reading, Math and Gym.</p>
+
+						<p>We have been lucky that she has a great teach in Ms. Williams.  She has made it an easy transition from a school where Aunt Cynthia is there 100% of the time (we were go grateful for the peace of mind that gave us) .  Of course, they keep track of everything digitally nowadays.  We have weekly (if not daily) "notifications" from Ms. Williams about what they're doing in school.  We get alerts when she does new art projects (and of course, there's an upsell to buy the coffee mug with your child's artwork on it - hah).</p>
+
+						<p>Maci's also does a lot of extra curricular activities.  From gymnastics, to ballet, to swimming to soccer, she keeps herself busy.  At the beginning of the year, I would've said soccer was her favorite sport, however as she progresses, she has found a calling in gymnastics.  She LOVES to play board games, and is VERY competitive.  She is a sore loser at times, which has been hard to correct - but I love the competitiveness in her.</p>
 
 						<p><strong><u>Lillian</u></strong></p>
 
-						<p>Lillian's year had a rough start.  She started it off by breaking her collar bone (she was jumping on our chaise lounge and took an ackward tumble).</p>
+						<p>Lillian's year had a rough start.  She started it off by breaking her collar bone (she was jumping on our chaise lounge and took an ackward tumble).  Initially, doctors were concerned because this was her second broken bone in as many years, but soon realized that she is just a clumsy Thompson :P  However, since then, she has been an unstoppable force.  She, too, has taken a big interest in gymnastics (I still cringe everytime she does a summersault). </p>
+
+						<p>Lillian also started soccer this year - which was a HUGE step (last year, she quit after the first time she got knocked down).  She has also taken a big step "socially" as well.  She now goes to a school where her big sister and favorite teach (Mrs. Sliman) are no longer there to help guide her on a day-to-day basis - and she is doing great.  Don't get me wrong, she is still a shy/coy young lady - but she has made a lot of new friends, and even went to her first birthday party at Chuck E Cheese.</p>
+
+						<p>In regards to her food allergy, she has gotten sick once or twice this year (because of accidental ingestion of gluten), however I feel like we (and she) has a much better feel about what her allergy is - and how to circumvent it.  It has been a big learning process for her mother and I (mostly her mother), but the first time you see her get sick off of gluten for a 6 hour spell, you do everything in your power to make sure it doesn't happen again</p> 
 
 						<p><strong><u>Jess</u></strong></p>
-						<p>Jess' uear has been more of the same</p>
+						<p>Jess has continuously been the engine of this train.  </p>
 
 						<p><strong><u>Me</u></strong></p>
-						<p>This has been a rollar coaster of a year for me.  </p>
+						<p>This has been a rollar coaster of a year for me.  As I'm sure you both know, I lost my sister this year.  That was one of the toughest times that I've had to endure in all of my life.  I have been fortunate enough that I have not had to see a lot of death throughout my life, however I have now learned that this inexperience has also made it harder for me to cope/grieve.  </p>
+
+						<p>Work has also been a roller coaster.  At times, I feel like I am the king of that place - I work hard, bosses love me, and I enjoy my coworkers.  However, there are times where I feel like I am not challenged and wearing too many hats (good at a lot of things, expert at nothing).  I have a great relationship with my direct boss (the President) with a fair salary and the flexibility to work form home, however I find myself disagreeing with a lot of decisions that my CEO makes.  Long story short, the benefits at this job have thus far outweighed the negatives - but, I plan to continue to think through throughout 2017.</p>
+
+						<p>Yet, this "void" that I seem to have from work has led me to learning new things outside of work.  I've recently grown a big interest in electrical engineering.  I've started building my own circuits, and really figuring out how certain electronics work (resistors, capacitors, inductors, etc..).  This year I've built two projects that I'm pretty proud of - my retro video game system (over 400+ games from Nintendo, Super Nintendo, Sega, Atari, Nintendo 64, etc..) and my "Alexa" (It's basically an <a href="http://www.amazon.com/echo" target="_blank">Amazon Echo</a> - I can talk/say commands and she will respond with answers).  I've actually sold about 20 retrov ideo game consoles so it's been exciting to see the interest.</p>
+
+						<p>... and I guess that about sums it up for me.  I want you to both know that I love you very much .. and want to wish you a very Merry Christmas!</p>
+
+						<p><strong>Matt</strong></p>
 
 					</div>
 					
