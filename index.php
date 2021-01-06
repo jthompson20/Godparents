a884ed5b45f5c472ce6706c3ac3d3124ca5d5ac6
Matt Thompson <thompson2091@gmail.com>
2021-01-06 16:29:47 -0500

    test

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>A Letter to My Godparents - New</title>
+<title>A Letter to My Godparents - New Stuff</title>
 
 <?php include('inc/head.php'); ?>
 
