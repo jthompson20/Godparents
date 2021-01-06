d55799000f386e4e81950645425911062e45b303
Matt Thompson <thompson2091@gmail.com>
2021-01-06 13:02:29 -0500

    hi mom

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>A Letter to My Godparents</title>
+<title>A Letter to My Godparents - HI MOM</title>
 
 <?php include('inc/head.php'); ?>
 
